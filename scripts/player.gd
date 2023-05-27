cd09d8410146bb24359247933523c12e8f196f14
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-05-27 22:47:19 +0700

    WIP player controller and test scene

---
+++ scripts/player.gd
@@ -0,0 +1,23 @@
+extends PhysicsBody2D
+
+
+@export var max_falling_speed := 0x5ff
+@export var gravity := 0x50
+@export var jumping_gravity := 0x20
+@export var jump_speed := 0x500
+
+var velocity: Vector2
+var is_on_ground: bool
+
+
+func _physics_process(delta: float):
+	if velocity.y > max_falling_speed:
+		velocity.y = max_falling_speed
+	if !Input.is_action_pressed("jump") || (velocity.y < 0):
+		velocity.y -= gravity
+	if Input.is_action_pressed("jump") && (velocity.y >= 0):
+		velocity.y -= jumping_gravity
+	if is_on_ground && Input.is_action_just_pressed("jump"):
+		velocity.y = jump_speed
+	
+	var collision := move_and_collide(Vector2(0, -velocity.y) / 0x200)
---
+++ scripts/utils.gd
@@ -0,0 +1,6 @@
+class_name Utils
+extends RefCounted
+
+
+static func to_pixels(unit: int) -> int:
+	return unit / 0x200
